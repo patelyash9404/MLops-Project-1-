0b82bf285e634c45c6bd6fe299c87dd96ee0601d
Yash Patel <yashpatel940516@gmail.com>
2026-02-11 10:09:09 +0530

    add the -e . to the requirement file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version='0.1.0',
+    author='Yash Patel',
+    author_email='yashpatel940516@gmail.com',
+    packages=find_packages()
+)
